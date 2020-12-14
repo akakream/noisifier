6f820bc48e4f8b80fbf5850202bfb8bdd58fde58
akakream <akakream@gmail.com>
2020-12-15 02:50:06 +0300

    forgot to add classes to init file

--- noisifier/__init__.py
+++ noisifier/__init__.py
@@ -3,4 +3,6 @@
 # Version of noisifier
 __version__ = '0.1'
 
-from noisifier.noisifier import Noisifier
+from noisifier.binary_class import Binary_Class_Noisifier
+from noisifier.multi_class import Multi_Class_Noisifier
+from noisifier.multi_label import Multi_Label_Noisifier
